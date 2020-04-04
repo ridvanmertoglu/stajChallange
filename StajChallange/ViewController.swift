dd4377dc316a3f00f9299c683f5fe28357346f7a
ridvanmertoglu <47368549+ridvanmertoglu@users.noreply.github.com>
2020-04-04 20:32:55 +0300

    Initial Commit

---
+++ StajChallange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StajChallange
+//
+//  Created by RIDVAN on 4.04.2020.
+//  Copyright © 2020 ridvanmertoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
